f21e86dddf4fb07466810d717179df1b115b24da
Shreyas Patel <61398641+371999@users.noreply.github.com>
2024-10-28 20:45:02 -0400

    Update main.tf

--- azure-terraform/main.tf
+++ azure-terraform/main.tf
@@ -1,16 +1,7 @@
 provider "azurerm" {
   features {}
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
 }
 
-# Define variables for sensitive information
-variable "client_id" {}
-variable "client_secret" {}
-variable "subscription_id" {}
-variable "tenant_id" {}
 variable "ssh_public_key" {}
 
 terraform {
