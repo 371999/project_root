f2821e278ee368a35430d2ce47e40fba570c7696
Shreyas Patel <61398641+371999@users.noreply.github.com>
2024-10-28 20:52:02 -0400

    Update main.tf

--- backend-setup/main.tf
+++ backend-setup/main.tf
@@ -1,6 +1,9 @@
 provider "azurerm" {
   features {}
+  client_id       = "dd2f42db-9fe4-4d32-9593-cdf7e597b07a"
+  client_secret   = "1mf8Q~N2x1sM_TiFDKCABEGVTgzlPVgCnyZFQb8b"
   subscription_id = "cd06d49d-6ae2-4d2b-82e4-50b2b98f55dd"
+  tenant_id       = "ed27b597-cea0-4942-8c6f-40e6a78bf47d"
 }
 
 resource "azurerm_resource_group" "tf_backend_rg" {
